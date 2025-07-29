fad669e2a05750626ac2bb9bfd4cac97298af813
kferjaoui <khalilferjaoui@yahoo.fr>
2025-07-29 22:24:10 +0200

    Completed the implementaion of sqrtAVX2()

--- prog4_sqrt/main.cpp
+++ prog4_sqrt/main.cpp
@@ -9,6 +9,7 @@
 using namespace ispc;
 
 extern void sqrtSerial(int N, float startGuess, float* values, float* output);
+extern void sqrtAVX2(int N, float startGuess, float* values, float* output);
 
 static void verifyResult(int N, float* result, float* gold) {
     for (int i=0; i<N; i++) {
@@ -34,17 +35,17 @@ int main() {
         // to you generate best and worse-case speedups
         
         // starter code populates array with random input values
-        // values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
         
         // Initial guess equal values
         // values[i] = initialGuess;
 
         // Only one element demands long compute
-        if (i==(N-1)){
-            values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
-        } else {
-            values[i] = initialGuess;
-        }
+        // if (i==(N-1)){
+        //     values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        // } else {
+        //     values[i] = initialGuess;
+        // }
     }
 
     // generate a gold version to check results
@@ -72,7 +73,7 @@ int main() {
     // time of three runs.
     //
     double minISPC = 1e30;
-    for (int i = 0; i < 30; ++i) {
+    for (int i = 0; i < 10; ++i) {
         double startTime = CycleTimer::currentSeconds();
         sqrt_ispc(N, initialGuess, values, output);
         double endTime = CycleTimer::currentSeconds();
@@ -91,7 +92,7 @@ int main() {
     // Tasking version of the ISPC code
     //
     double minTaskISPC = 1e30;
-    for (int i = 0; i < 30; ++i) {
+    for (int i = 0; i < 10; ++i) {
         double startTime = CycleTimer::currentSeconds();
         sqrt_ispc_withtasks(N, initialGuess, values, output);
         double endTime = CycleTimer::currentSeconds();
@@ -102,8 +103,28 @@ int main() {
 
     verifyResult(N, output, gold);
 
+    // Clear out the buffer
+    for (unsigned int i = 0; i < N; ++i)
+        output[i] = 0;
+
+    //
+    // Tasking version of the ISPC code
+    //
+    double minAVX2 = 1e30;
+    for (int i = 0; i < 10; ++i) {
+        double startTime = CycleTimer::currentSeconds();
+        sqrtAVX2(N, initialGuess, values, output);
+        double endTime = CycleTimer::currentSeconds();
+        minAVX2 = std::min(minAVX2, endTime - startTime);
+    }
+
+    printf("[sqrt task AVX2]:\t[%.3f] ms\n", minAVX2 * 1000);
+
+    verifyResult(N, output, gold);
+
     printf("\t\t\t\t(%.2fx speedup from ISPC)\n", minSerial/minISPC);
     printf("\t\t\t\t(%.2fx speedup from task ISPC)\n", minSerial/minTaskISPC);
+    printf("\t\t\t\t(%.2fx speedup from AVX2)\n", minSerial/minAVX2);
 
     delete [] values;
     delete [] output;
--- prog4_sqrt/sqrtAVX2.cpp
+++ prog4_sqrt/sqrtAVX2.cpp
@@ -15,26 +15,37 @@ void sqrtAVX2(int N,
     __m256 const one = _mm256_set1_ps(1.f);
     __m256 const three = _mm256_set1_ps(3.f);
 
+    //  __m256 result = _mm256_loadu_ps(&output[i]);
+
     for (int i=0; i<N; i+=8) {
 
         __m256 x = _mm256_loadu_ps(&values[i]);
         __m256 guess = _mm256_set1_ps(initialGuess);
 
         __m256 error = _mm256_sub_ps(_mm256_mul_ps( guess,_mm256_mul_ps(guess, x) ), one);
-        // TODO: Apply abs to error
+
+        // Apply abs to error: 
+        // Bitwise AND opetaion to convert the sign bit (bit 31) of each of the 8 32-bit floats into a 0 i.e. positive
+        error = _mm256_and_ps(error, _mm256_castsi256_ps(_mm256_set1_epi32(0x7FFFFFFF)));
         
-        // TODO: Corret illegal comparison below
-        while (error > kThreshold) {
+        // mask is equal to zero only if condition error > kThreshols is false for all the gang lanes
+        int mask = _mm256_movemask_ps(_mm256_cmp_ps(error, kThreshold, _CMP_GT_OQ));
+        while (mask != 0) {
             __m256 three_guess = _mm256_mul_ps(three , guess);
             __m256 g2 = _mm256_mul_ps(guess , guess);
             __m256 g3 = _mm256_mul_ps(g2 , guess);
             __m256 xg3 = _mm256_mul_ps(x , g3);
 
             guess = _mm256_mul_ps(_mm256_sub_ps( three_guess , xg3) , half );
-            error = _mm256_abs_epi32( _mm256_sub_ps( _mm256_mul_ps( x, _mm256_mul_ps(guess , guess) ), one) );
-            // TODO: Apply abs to error
+            error = _mm256_sub_ps( _mm256_mul_ps( x, _mm256_mul_ps(guess , guess) ), one) ;
+            
+            // Absolute value
+            error = _mm256_and_ps(error, _mm256_castsi256_ps(_mm256_set1_epi32(0x7FFFFFFF)));
+            
+            // Update mask 
+            mask = _mm256_movemask_ps(_mm256_cmp_ps(error, kThreshold, _CMP_GT_OQ));
         }
         
-        __m256_storeu_ps(&output[i] , _mm256_mul_ps(x, guess));
+        _mm256_storeu_ps(&output[i] , _mm256_mul_ps(x, guess));
     }
 }
